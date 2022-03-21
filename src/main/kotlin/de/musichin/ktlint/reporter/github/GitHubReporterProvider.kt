5c7b6c3ebf528e8a64bb586b646193be005e36d2
Anton Musichin <musichin@users.noreply.github.com>
2022-03-21 23:03:37 +0100

    upgrades ktlint to version 0.45.0 (#16)

--- src/main/kotlin/de/musichin/ktlint/reporter/github/GitHubReporterProvider.kt
+++ src/main/kotlin/de/musichin/ktlint/reporter/github/GitHubReporterProvider.kt
@@ -3,7 +3,7 @@ package de.musichin.ktlint.reporter.github
 import com.pinterest.ktlint.core.ReporterProvider
 import java.io.PrintStream
 
-class GitHubReporterProvider : ReporterProvider {
+class GitHubReporterProvider : ReporterProvider<GitHubReporter> {
     override val id: String = "github"
 
     override fun get(out: PrintStream, opt: Map<String, String>): GitHubReporter {
