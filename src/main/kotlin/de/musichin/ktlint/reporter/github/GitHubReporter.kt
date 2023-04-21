0a1debe3d788294591c4f78eb3b3152bc6e0f1d9
Anton Musichin <musichin@users.noreply.github.com>
2023-04-21 21:40:42 +0200

    upgrades ktlint to version 0.49.0 (#53)

--- src/main/kotlin/de/musichin/ktlint/reporter/github/GitHubReporter.kt
+++ src/main/kotlin/de/musichin/ktlint/reporter/github/GitHubReporter.kt
@@ -1,21 +1,22 @@
 package de.musichin.ktlint.reporter.github
 
-import com.pinterest.ktlint.core.LintError
-import com.pinterest.ktlint.core.Reporter
+import com.pinterest.ktlint.cli.reporter.core.api.KtlintCliError
+import com.pinterest.ktlint.cli.reporter.core.api.KtlintCliError.Status.FORMAT_IS_AUTOCORRECTED
+import com.pinterest.ktlint.cli.reporter.core.api.ReporterV2
 import java.io.PrintStream
 
 class GitHubReporter(
     private val out: PrintStream,
     val level: Level = Level.ERROR,
-) : Reporter {
+) : ReporterV2 {
 
-    override fun onLintError(file: String, err: LintError, corrected: Boolean) {
-        if (corrected) return
+    override fun onLintError(file: String, ktlintCliError: KtlintCliError) {
         if (level == Level.NONE) return
+        if (ktlintCliError.status == FORMAT_IS_AUTOCORRECTED) return
 
-        val line = err.line
-        val column = err.col
-        val message = escape(err.detail)
+        val line = ktlintCliError.line
+        val column = ktlintCliError.col
+        val message = escape(ktlintCliError.detail)
         val command = level.name.lowercase()
         out.println("::$command file=$file,line=$line,col=$column::$message")
     }
